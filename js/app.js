6445b8d7f5e8f638e5b116e78ffddef15e6ac0cd
Hossen <mdmitho199911@gmail.com>
2022-02-17 14:17:52 +0600

    all js done

--- js/app.js
+++ js/app.js
@@ -3,65 +3,73 @@ function getInputValue(inputId){
     const inputField = document.getElementById(inputId);
     const inputAmountText = inputField.value;
     const inputValue = parseFloat(inputAmountText);
-    inputField.value='';
+    // inputField.value='';
     return inputValue;
+}
+function Expensescalculate(){
+    const foodAmonut = getInputValue("food-input")
+    const rentAmount = getInputValue('rent-input')
+    const clothesAmount = getInputValue('clothes-input')
+    
+
+    if(foodAmonut<0 || rentAmount<0 || clothesAmount<0 ){
+        alert('Please Enter a Positive Number')
+    }
+else{
+    const expensesAmount = foodAmonut+rentAmount+clothesAmount;
+    return expensesAmount;
+}
+}
+function totalBalance(){
+   
+    const incomeAmonut = getInputValue("income-input")
+    const totalCalculate= Expensescalculate();
+
+    if(incomeAmonut<0){
+        alert('Please Enter a Positive Number')
+    }
+    else{
+        const BalanceAmount = incomeAmonut-totalCalculate;
+        return BalanceAmount;
+    }
 }
 document.getElementById('calculate-btn').addEventListener('click',function(){
 
-
-   let BalanceTotal=document.getElementById('Balance')
    const totalExpenses = document.getElementById('total-expenses');
-   const incomeAmonut = getInputValue("income-input")
-   const foodAmonut = getInputValue("food-input")
-   const rentAmount = getInputValue('rent-input')
-   const clothesAmount = getInputValue('clothes-input')
-
-   const expensesAmount = foodAmonut+rentAmount+clothesAmount;
-   const totalBalance = incomeAmonut-expensesAmount;
-
-   if(expensesAmount>totalBalance){
-   alert('You have Expenses too much money')
-   }
-   else{
-    if(foodAmonut>0 && rentAmount>0 && clothesAmount>0 ){
-        totalExpenses.innerText = expensesAmount;
-        if(totalBalance>0){
-            BalanceTotal.innerText = totalBalance;
-        }
-        else{
-            alert('Please Enter a Positive Number')
-          }
-       
-       }
-       else{
-         alert('Please Enter a Positive Number')
-       }
-   }
-
-
+   const totalCalculate= Expensescalculate();
+   totalExpenses.innerText =totalCalculate;
+   const BalanceTotal=document.getElementById('Balance')
+   const balance=totalBalance();
+   BalanceTotal.innerText=balance;
 })
 
-function expensesTotal(){
-    const incomeAmonut = getInputValue("income-input")
-    const foodAmonut = getInputValue("food-input")
-   const rentAmount = getInputValue('rent-input')
-   const clothesAmount = getInputValue('clothes-input')
-   const expensesTotalAmount = foodAmonut+rentAmount+clothesAmount;
-   const totalAmount = incomeAmonut-expensesTotalAmount;
-   return totalAmount;
-}
-
 document.getElementById('save-btn').addEventListener('click',function(){
-    const incomeAmonut = getInputValue("income-input")
     const saveingAmount =document.getElementById("saving-amount")
     const remainingAmount =document.getElementById("remaining-balance")
+    const incomeAmonut = getInputValue("income-input")
     const saveInput = getInputValue("save-input")
- const savingAmountTotal =(incomeAmonut*saveInput)/100;
-    const  totalBalance = expensesTotal()
- const previousBalance = totalBalance-savingAmountTotal;
- console.log(previousBalance)
-   saveingAmount.innerText =savingAmountTotal;
-  
-   const remainingBalance =totalBalance-savingAmountTotal;
-//    remainingAmount.innerText =previousBalance;
+    if(saveInput<0){
+        alert('Please Enter a Positive Number')
+    }
+    else{
+        const savingAmountTotal =(incomeAmonut*saveInput)/100;
+        saveingAmount.innerText =savingAmountTotal;
+        const previousBalanceTotal = totalBalance()
+        const remainingBalance =previousBalanceTotal-savingAmountTotal;
+        remainingAmount.innerText=remainingBalance;
+    }
+
+    const incomeField = document.getElementById('income-input')
+         incomeField.value='';
+    const foodField = document.getElementById('food-input')
+         foodField.value='';
+    const rentField = document.getElementById('rent-input')
+         rentField.value='';
+    const clothesField = document.getElementById('clothes-input')
+         clothesField.value='';
+    const saveField = document.getElementById('save-input')
+         saveField.value='';
+
+
+
 })
